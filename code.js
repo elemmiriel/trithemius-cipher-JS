4e256f47b0f1b9a9a3df6e0d9206206b68ede2f2
elemmiriel <79629758525@yandex.ru>
2017-11-16 17:08:28 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,25 +1,27 @@
 var alphabet = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я','а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я','0','1','2','3','4','5','6','7','8','9',' ',',','.','!','?',';'];
+var n = alphabet.length; //n - мощность алфавита
 
+//шифруем
 function cipher () {
-  var n = alphabet.length; //n - мощность алфавита
   var k = document.getElementById("key").value; //значение поля секретный ключ
   var textInput = document.getElementById("input-text").value; //текст из поля Исходный текст
   var textOutput = document.getElementById("output-text");
   textInput.split(/(\w)/i); //Разбиваем исходный текст на массив посимвольно
-  var str= ""; //Строковая переменная для вывода
+  var str=""; //Строковая переменная для вывода
   var m = -1;
-  
-  console.log(textInput); //для контроля
+  console.log(textInput);
   
   for (i = 0; i < textInput.length; i++){
-    m= find(alphabet, textInput[i]); //Функция find() вычисляет индекс текущего символа в массиве алфавита
-    
-    if (m!==-1) {     
-        console.log("m=" + m); //для контроля
-        console.log("k=" + k); //для контроля
-        str = str + alphabet[ (m + k) % n ]; //Строка вывода формируется посимвольно. Мат. модель см. в википедии.
-        console.log(alphabet[ (m + k) % n ] ); //для контроля  
-    }else{ //Отсутствующие в алфавите программы символы заменяются нижним подчёркиванием.
+    m= find(alphabet,textInput[i]);
+      if (m!==-1) {
+////////////////////////////////////////        
+        /*  Здесь можно пошагово увидеть значения переменных */
+        console.log("m=" + m);
+        console.log("k=" + k);
+//////////////////////////////////////
+        str = str + alphabet[(m+k)%n]; //Строка вывода формируется посимвольно. Мат. модель см. в википедии.
+        console.log(alphabet[(m+k)%n]);
+      }else{ //Отсутствующие в алфавите программы символы заменяются нижним подчёркиванием.
       str = str + '_';
     }
   }
@@ -27,6 +29,55 @@ function cipher () {
   textOutput.innerHTML = str;
 }
 
+
+//дешифровка
+function decipher () {
+  var k = document.getElementById("key").value; //значение поля секретный ключ
+  var textInput = document.getElementById("input-text").value; //текст из поля Исходный текст
+  var textOutput = document.getElementById("output-text");
+  textInput.split(/(\w)/i); //Разбиваем исходный текст на массив посимвольно
+  var str=""; //Строковая переменная для вывода
+  var m = -1;
+  console.log(textInput);
+/*  
+  for (i = 0; i < textInput.length; i++){
+    m= find(alphabet,textInput[i]);
+      if (m!==-1) {
+////////////////////////////////////////        
+      //Здесь можно пошагово увидеть значения переменных 
+        console.log("m=" + m);
+        console.log("k=" + k);
+//////////////////////////////////////
+
+        while((m-k)<0){
+          m+=n;
+          console.log("m+=n -> "+m)
+        }        
+        console.log((m-k)%n);
+        str = str + alphabet[(m-k)%n]; //Строка вывода формируется посимвольно. Мат. модель см. в википедии.
+        console.log(alphabet[(m-k)%n]);
+      }else{ //Отсутствующие в алфавите программы символы заменяются нижним подчёркиванием.
+      str = str + '_';
+    }
+  }
+*/
+  for (i = 0; i < textInput.length; i++){
+    m= find(alphabet,textInput[i]);
+       
+        console.log(m*n-k);
+        str = str + alphabet[m*n-k]; //Строка вывода формируется посимвольно. Мат. модель см. в википедии.
+        console.log(alphabet[m*n-k]);
+    
+  }
+        
+  
+  console.log(str);
+  textOutput.innerHTML = ""; //не обязательно
+  textOutput.innerHTML = str;
+}
+
+
+//Функция find() вычисляет индекс символа в массиве алфавита
 function find(array, value) {
   if (array.indexOf) { // если метод существует
     return array.indexOf(value);
